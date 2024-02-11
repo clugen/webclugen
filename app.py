ef47f3e569407432b32da240bbb9af30d580fba0
Nuno Fachada <nuno.fachada@ulusofona.pt>
2024-02-11 00:38:41 +0000

    Bootstrap styling + Conf. num. clusters and points

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
-from dash import Dash, html, dcc, Input, Output, callback
+from dash import Dash, html, dcc, Input, State, Output, callback
+import dash_bootstrap_components as dbc
 import plotly.express as px
 from pyclugen import clugen
 import numpy as np
 
 
-app = Dash(__name__)
+external_stylesheets = [dbc.themes.FLATLY]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
+app.layout = dbc.Container([
     html.H1(children='WebClugen', style={'textAlign':'center'}),
-    dcc.Input(id='seed', placeholder='Enter seed...', type='number', min=0, max=np.iinfo(np.int32).max, step=1),
+    dbc.Label('Number of clusters'),
+    dbc.Input(id='num_clusters', debounce=True, value=4, type='number', min=1, max=30, step=1),
+    dbc.Label('Number of points'),
+    dbc.Input(id='num_points', debounce=True, value=500, type='number', min=1, max=50000, step=1),
+    # dbc.Label('Direction'),
+    # dbc.Label('Angle dispersion'),
+    # dbc.Label('Cluster separation'),
+    # dbc.Label('Line length'),
+    # dbc.Label('Line length dispersion'),
+    # dbc.Label('Lateral dispersion'),
+    dbc.Label('Seed'),
+    dbc.Input(id='seed', debounce=True, value=0, type='number', min=0, max=np.iinfo(np.int32).max, step=1),
+    dbc.Button("Generate", id="gen-button", color="primary", className="me-1"),
     dcc.Graph(id='plot', figure={})
 ])
 
 # Add controls to build the interaction
 @callback(
-    Output(component_id='plot', component_property='figure'),
-    Input(component_id='seed', component_property='value')
+    Output('plot', 'figure'),
+    Input('gen-button', 'n_clicks'),
+    State('num_clusters', 'value'),
+    State('num_points', 'value'),
+    State('seed', 'value')
 )
-def update_graph(seed):
-    out2d = clugen(2, 4, 400, [1, 0], 0.4, [50, 10], 20, 1, 2, rng=seed)
+def update_graph(n_clicks, num_clusters, num_points, seed):
+    out2d = clugen(2, num_clusters, num_points, [1, 0], 0.4, [50, 10], 20, 1, 2, rng=seed)
     fig = px.scatter(x=out2d.points[:, 0], y=out2d.points[:, 1], color=out2d.clusters.astype(str))
     return fig
 
